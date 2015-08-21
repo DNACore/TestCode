ccf96abf49574dbd09daac6728fc9030fc85579e
Encoder <Encoder@zp.lan>
2015-08-21 17:34:08 +0800

    添加全局预处理文件

--- TestCode/TestCode/functions/TrafficCount/TrafficCountViewController.m
+++ TestCode/TestCode/functions/TrafficCount/TrafficCountViewController.m
@@ -43,29 +43,23 @@ int getGprs3GFlowIOBytes() {//C函数命名方式
     struct ifaddrs *ifa_list= 0, *ifa;
     
     if (getifaddrs(&ifa_list)== -1) {
-        
         return 0;
         
     }
     
     uint32_t iBytes =0;
-    
     uint32_t oBytes =0;
     
     for (ifa = ifa_list; ifa; ifa = ifa->ifa_next)
-        
     {
         
         if (AF_LINK!= ifa->ifa_addr->sa_family)
-            
             continue;
         
         if (!(ifa->ifa_flags& IFF_UP) &&!(ifa->ifa_flags& IFF_RUNNING))
-            
             continue;
         
         if (ifa->ifa_data== 0)
-            
             continue;
         
         if (!strcmp(ifa->ifa_name,"pdp_ip0")) {
@@ -73,7 +67,6 @@ int getGprs3GFlowIOBytes() {//C函数命名方式
             struct if_data *if_data = (struct if_data*)ifa->ifa_data;
             
             iBytes += if_data->ifi_ibytes;
-            
             oBytes += if_data->ifi_obytes;
             
             NSLog(@"%s :iBytes is %d, oBytes is %d",ifa->ifa_name, iBytes, oBytes);
@@ -94,27 +87,22 @@ int getGprs3GFlowIOBytes() {//C函数命名方式
     struct ifaddrs *ifa_list = 0, *ifa;
     
     if (getifaddrs(&ifa_list) == -1) {
-        
         return 0;
         
     }
     
     uint32_t iBytes = 0;
-    
     uint32_t oBytes = 0;
     
     for (ifa = ifa_list; ifa; ifa = ifa->ifa_next) {
         
         if (AF_LINK != ifa->ifa_addr->sa_family)
-            
             continue;
         
         if (!(ifa->ifa_flags & IFF_UP) && !(ifa->ifa_flags & IFF_RUNNING))
-            
             continue;
         
         if (ifa->ifa_data == 0)
-            
             continue;
         
         /* Not a loopback device. */
@@ -122,15 +110,10 @@ int getGprs3GFlowIOBytes() {//C函数命名方式
         if (strncmp(ifa->ifa_name, "lo", 2))
             
         {
-            
             struct if_data *if_data = (struct if_data *)ifa->ifa_data;
-            
             iBytes += if_data->ifi_ibytes;
-            
             oBytes += if_data->ifi_obytes;
-            
             //            NSLog(@"%s :iBytes is %d, oBytes is %d",
-            
             //                  ifa->ifa_name, iBytes, oBytes);
             
         }
@@ -145,35 +128,25 @@ int getGprs3GFlowIOBytes() {//C函数命名方式
 
 //单位换算
 NSString* bytesToAvaiUnit(int bytes) {//C语言命名方式
-    
     if(bytes < 1024)  // B
     {
-        
         return [NSString stringWithFormat:@"%dB", bytes];
-        
     }
     
     else if(bytes >= 1024 && bytes < 1024 * 1024) // KB
     {
-        
         return [NSString stringWithFormat:@"%.1fKB", (double)bytes / 1024];
-        
     }
     
     else if(bytes >= 1024 * 1024 && bytes < 1024 * 1024 * 1024) // MB
     {
-        
         return [NSString stringWithFormat:@"%.2fMB", (double)bytes / (1024 * 1024)];
-        
     }
     
     else // GB
     {
-        
         return [NSString stringWithFormat:@"%.3fGB", (double)bytes / (1024 * 1024 * 1024)];
-        
     }
-    
 }
 
 @end
